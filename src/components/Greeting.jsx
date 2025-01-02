c0bee741c44da8481f42df285399ffbdca5878e8
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-01-02 19:41:04 +0530

    Commit

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const Greeting = () => {
+    const [name, setName] = useState("")
+  return (
+    <div>
+        
+            <h1>Enter Your name</h1>
+            <input type="text" onChange={(e) => {
+                setName(e.target.value)
+            }} />
+            <p>{name.length ? `Hello ${name} !` : ""} </p>
+        
+    </div>
+  )
+}
+
+export default Greeting
